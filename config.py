6d8ac5cec92a60678ede0b31baa3d30615c93289
Lautaro Bachmann <laurigbach@gmail.com>
2021-12-21 14:43:51 -0300

    Changed default config

--- config.py
+++ config.py
@@ -3,10 +3,15 @@ from collections import namedtuple
 Config = namedtuple('Config', ["subreddits", "bot_name", "keywords", "notify_command", "timeout"])
 
 bot_config = Config(
-            subreddits=['slavelabour', 'forhire', 'sellmyskill', 'freelance_forhire', 'Jobs4Scrapers', 'Jobs4Bitcoins', 'DeveloperJobs', 'jobbit'],
-            bot_name="Python notification bot",
-            keywords=['hiring', 'task'],
-            notify_command="dunstify -t 0".split(),
-            timeout=300,
+            subreddits=[
+                'slavelabour', 'forhire', 'sellmyskill',
+                'freelance_forhire', 'Jobs4Scrapers', 'Jobs4Bitcoins',
+                'DeveloperJobs', 'jobbit'
+            ], # Subreddits to scrape
+            bot_name="Name of the bot", # User agent
+            keywords=['hiring', 'task'], # keywords that have to be in post
+            notify_command="notify-send -t 0", # Command to send the notification 
+            timeout=300, #seconds
 )
 
+#"dunstify -t 0",
