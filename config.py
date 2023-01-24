24b0331e63e89129aedecc58ae253648eb850c8c
Lautaro Bachmann <laurigbach@gmail.com>
2023-01-24 12:00:13 -0300

    Applied formatting and added max_time_since_creation filter

--- config.py
+++ config.py
@@ -1,17 +1,36 @@
 from collections import namedtuple
 
-Config = namedtuple('Config', ["subreddits", "bot_name", "keywords", "notify_command", "timeout"])
-
-bot_config = Config(
-            subreddits=[
-                'slavelabour', 'forhire', 'sellmyskill',
-                'freelance_forhire', 'Jobs4Scrapers', 'Jobs4Bitcoins',
-                'DeveloperJobs', 'jobbit'
-            ], # Subreddits to scrape
-            bot_name="Name of the bot", # User agent
-            keywords=['hiring', 'task'], # keywords that have to be in post
-            notify_command="notify-send -t 0", # Command to send the notification 
-            timeout=300, #seconds
+Config = namedtuple(
+    "Config",
+    [
+        "subreddits",
+        "bot_name",
+        "keywords",
+        "notify_command",
+        "timeout",
+        "max_time_since_creation",
+    ],
 )
 
-#"dunstify -t 0",
+bot_config = Config(
+    subreddits=[
+        "slavelabour",
+        "forhire",
+        "sellmyskill",
+        "freelance_forhire",
+        "Jobs4Scrapers",
+        "Jobs4Bitcoins",
+        "DeveloperJobs",
+        "jobbit",
+    ],  # Subreddits to scrape
+    bot_name="Custom Reddit Notification Bot",  # User agent
+    keywords=[
+        "[hiring]",
+        "[task]",
+    ],  # keywords that have to be in post, write without spaces
+    notify_command="dunstify -t 0",  # Command to send the notification
+    timeout=600,  # seconds
+    max_time_since_creation=600,  # Max time since creation in minutes
+)
+
+# "dunstify -t 0",
